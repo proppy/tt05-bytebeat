5bbe81ebd11efea3e0ce1432ac0d0fb6544b0cc9
Johan Euphrosine <proppy@google.com>
2023-11-01 21:07:10 +0900

    src/test: fix dut handles

--- src/test.py
+++ src/test.py
@@ -1,22 +1,24 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+from cocotb.triggers import ClockCycles
+
+FREQ = 8000
 
 @cocotb.test()
-async def test_my_design(dut):
+async def test_bytebeat(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 1, units="ms")
-    cocotb.start_soon(clock.start())    
+    clock = Clock(dut.clk, (1.0/FREQ), units="sec")
+    cocotb.start_soon(clock.start())
 
     dut.rst_n.value = 0 # low to reset
-    dut.ui_in = 0x57 # a=5 b=7
-    dut.uio_in = 0x3a # c=3 d=10
+    dut.ui_in.value = 0x57  # a=5 b=7
+    dut.uio_in.value = 0x3a # c=3 d=10
     await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 1 # take out of reset    
+    dut.rst_n.value = 1 # take out of reset
     await ClockCycles(dut.clk, 65536)
     dut.rst_n.value = 0 # low to reset
-    dut.ui_in = 0x36 # a=3 b=6
-    dut.uio_in = 0x29 # c=2 d=9
+    dut.ui_in.value = 0x36  # a=3 b=6
+    dut.uio_in.value = 0x29 # c=2 d=9
     await ClockCycles(dut.clk, 1)
-    dut.rst_n.value = 1 # take out of reset    
+    dut.rst_n.value = 1 # take out of reset
     await ClockCycles(dut.clk, 65536)
